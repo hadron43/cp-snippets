6d24114cd4e3da456e012d3b608642522d81929a
Harsh Kumar <hadron43@yahoo.com>
2021-01-19 12:56:02 +0530

    codetostring: convert code to string easily for snippets

---
+++ codetostring.cpp
@@ -0,0 +1,60 @@
+/*
+ * Author : @hadron43
+ * Email : hadron43@yahoo.com
+ * 
+ * CPP to JSON Converter
+ * This prgram does the following task:
+ * 
+ * Read from input.cpp =>
+ * Parse to json string =>
+ * store it in output.json
+ */
+#include <bits/stdc++.h>
+#include <fstream>
+using namespace std;
+
+string line;
+
+ifstream in;
+ofstream out;
+
+void open_files() {
+    in.open("input.txt");
+    out.open("output.txt");
+}
+
+void close_files() {
+    in.close();
+    out.close();
+}
+
+void fetch_next() {
+    getline(in, line);
+    // cerr<<"Fetched: "<<line<<endl;
+}
+
+void flush(string end = "\n") {
+    line = line + end;
+    out<<line;
+}
+
+int main() {
+    open_files();
+
+    line = "[";
+    flush();
+    while(in) {
+        fetch_next();
+        string temp = "";
+        for(char ch : line) {
+            if(ch == 9)
+                temp += "\\t";
+            else
+                temp += ch;
+        }
+        line = "\t\"" + temp + "\"";
+        (in) ? flush(",\n") : flush();
+    }
+    line = "]";
+    flush();
+}
